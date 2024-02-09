6cce25f92bb93f17288504cd757d229a5a96570d
Yun Aleksandr Vyacheslavovich <alyun@ozon.ru>
2024-02-09 23:38:53 +0500

    Code was fixed

--- src/view/empty-list-message.js
+++ src/view/empty-list-message.js
@@ -1,14 +1,23 @@
 import AbstractView from '../framework/view/abstract-view.js';
-import { FILTER_VARIANTS, EMPTY_LIST_MESSAGE_BY_FILTERS_MAP } from '../const.js';
+import { FILTER_VARIANTS, EMPTY_LIST_MESSAGE_BY_FILTERS_MAP, FAILED_TO_LOAD_MESSAGE } from '../const.js';
 
 function createEmptyListMessageTemplate(message) {
   return `<p class="trip-events__msg">${message}</p>`;
 }
 
 export default class EmptyListMessage extends AbstractView {
+  #isLoadFailed = false;
+  #selectedFilter = null;
+
+  constructor({ isLoadFailed, selectedFilter }) {
+    super();
+    this.#isLoadFailed = isLoadFailed;
+    this.#selectedFilter = selectedFilter;
+  }
+
   get template() {
     const checkedFilter = FILTER_VARIANTS.find((filter) => filter.state === 'checked');
-    const message = EMPTY_LIST_MESSAGE_BY_FILTERS_MAP[checkedFilter.type];
+    const message = this.#isLoadFailed ? FAILED_TO_LOAD_MESSAGE : EMPTY_LIST_MESSAGE_BY_FILTERS_MAP[checkedFilter.type];
     return createEmptyListMessageTemplate(message);
   }
 }
